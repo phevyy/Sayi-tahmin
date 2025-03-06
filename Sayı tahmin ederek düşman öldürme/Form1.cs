38919fbf4a4882e5212cddf1eb9a58c30f1880c2
Fikret Emre Sınmaz <fikreddemre@gmail.com>
2025-03-07 02:21:10 +0300

    Sayi-tahmin
    Label ve textbox ile sayı tahmin ederek düşman öldürme oyunu

---
+++ Sayı tahmin ederek düşman öldürme/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Reflection.Emit;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace NDP
+{
+    public partial class Form1 : Form
+    {
+        private int dusmanNumarasi;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            YeniOyunBaslat();
+        }
+
+        private void YeniOyunBaslat()
+        {
+            Random rnd = new Random();
+            dusmanNumarasi = rnd.Next(1, 101); 
+            label2.Text = ""; 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int hedef;
+            if (int.TryParse(textBox1.Text, out hedef))
+            {
+                
+                if (hedef < 1 || hedef > 100)
+                {
+                    label2.Text = "Lütfen 1 ile 100 arasında bir sayı girin.";
+                    return; 
+                }
+
+                if (hedef == dusmanNumarasi)
+                {
+                    label2.Text = "Tebrikler! Düşmanı vurmayı başardınız.";
+                }
+                else
+                {
+                    
+                    DialogResult result = MessageBox.Show("Düşman kaçtı! Tekrar denemek ister misiniz?",
+                                                          "Yanlış Tahmin",
+                                                          MessageBoxButtons.YesNo,
+                                                          MessageBoxIcon.Question);
+                    if (result == DialogResult.Yes)
+                    {
+                        label2.Text = ""; 
+                        textBox1.Clear(); 
+                    }
+                    else
+                    {
+                        label2.Text = "Oyun bitti. Tekrar başlatmak için formu kapatıp açabilirsiniz.";
+                    }
+                }
+            }
+            else
+            {
+                label2.Text = "Lütfen geçerli bir düşman numarası girin.";
+            }
+        }
+    }
+}
